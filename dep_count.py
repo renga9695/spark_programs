f009c20d709e57e06ca4540d64eb115fc53bd707
renga9695 <renga9695@gmail.com>
2019-11-23 07:42:32 +0530

    dep_count version 2

--- dep_count.py
+++ dep_count.py
@@ -8,12 +8,14 @@ output=(sys.argv[3])
 
 ab= SparkConf().setMaster("yarn-client").setAppName("departmentCount")
 renga= SparkContext(conf=ab)
+
 amma= StreamingContext(renga,10)
-amma.start()
+
 from operator import add
 file= amma.socketTextStream(hostname,port)
 appa=file.filter(lambda f: f.split(" ")[6].split("/"))
 raja=appa.map(lambda m: (m.appa,1))
 r_gopal= raja.reduceByKey(add)
 r_gopal.saveAsTextFiles(output)
+amma.start()
 amma.awaitTermination()
