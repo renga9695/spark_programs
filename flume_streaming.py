2e7ae66772de3127feefd313158dece68489ffa9
renga9695 <52701372+renga9695@users.noreply.github.com>
2019-12-02 18:04:17 +0530

    version 2.0

--- flume_streaming.py
+++ flume_streaming.py
@@ -1,5 +1,21 @@
+from pyspark import SparkConf,SparkContext
 from pyspark.sql import SparkSession
 from pyspark.streaming import StreamingContext
+from pyspark.streaming.flume import FlumeUtils
 import sys
+from operator import add
 from pyspark.sql import functions
-i
+hostname= sys.argv[1]
+port= sys.argv[2]
+output=sys.argv[3]
+renga = SparkConf().setMaster("local").setAppName("karthik")
+amma= SparkContext(conf=renga)
+
+appa=StreamingContext(amma,30)
+rajagopal= FlumeUtils.createStream(appa,hostname,port)
+
+kowsi= rajagopal.flatMap(lambda fm : fm.split(" ")[6] == "deparment")
+ujesh= kowsi.map(lambda m : (m.split(" ")[6].split("/")[1],1))
+balaji=ujesh.reduceByKey(add)
+
+balaji.saveAsTextFiles(output)
