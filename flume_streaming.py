cc50c4c7ddf5206c8dba15c17363d895c71884f1
renga9695 <52701372+renga9695@users.noreply.github.com>
2019-12-03 17:51:59 +0530

    version 2.11

--- flume_streaming.py
+++ flume_streaming.py
@@ -1,5 +1,6 @@
 from pyspark import SparkConf,SparkContext
 from pyspark.streaming import StreamingContext
+from pyspark.storagelevel import StorageLevel
 from pyspark.streaming.flume import FlumeUtils
 import sys
 from operator import add
@@ -10,7 +11,7 @@ renga = SparkConf().setMaster("yarn-client").setAppName("karthik")
 amma= SparkContext(conf=renga)
 appa=StreamingContext(amma,30)
 addresses= [(hostname,port)]
-rajagopal= FlumeUtils.createPollingStream(appa,addresses)
+rajagopal= FlumeUtils.createPollingStream(appa,addresses,storageLevel=StorageLevel.MEMORY_AND_DISK_2)
 mohan= rajagopal.map(lambda m:m[1])
 kowsi= mohan.flatMap(lambda fm : fm.split(" ")[6] == "deparment")
 ujesh= kowsi.map(lambda m : (m.split(" ")[6].split("/")[1],1))
