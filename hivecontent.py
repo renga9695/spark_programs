dbcb4754c86112e3da55a5448b4f8987f0e74ff3
renga9695 <52701372+renga9695@users.noreply.github.com>
2019-12-12 20:37:00 +0530

    version 1.5

--- hivecontent.py
+++ hivecontent.py
@@ -1,8 +1,13 @@
 from pyspark.sql import SparkSession
-from pyspark.sql import HiveContext
 
-renga = SparkSession.builder.appName("ahamed").master("yarn-client").enableHiveSupport().getOrCreate()
-appa= HiveContext(renga)
+
+appa = SparkSession.\
+    builder.\
+    appName("ahamed").\
+    master("yarn-client").\
+    config("sspark.sql.warehouse.dir","hdfs://nn01.itversity.com:8020/apps/hive/warehouse/jarvis.db/").\
+    getOrCreate()
+
 
 appa.sql("use jarvis")
 amma= appa.sql('select * from crime3 where primary_type = "THEFT" limit 10')
