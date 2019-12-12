9b7f364055c55e687aaf2f8da48058989e766443
renga9695 <52701372+renga9695@users.noreply.github.com>
2019-12-12 21:04:09 +0530

    version 1.8

--- hivecontent.py
+++ hivecontent.py
@@ -5,6 +5,7 @@ appa = SparkSession.\
     builder.\
     appName("ahamed").\
     master("yarn-client").\
+    config("hive.metastore.uris","thrift://rm01.itversity.com:9083").\
     config("spark.sql.warehouse.dir","hdfs://nn01.itversity.com:8020/apps/hive/warehouse/").\
     getOrCreate()
 
