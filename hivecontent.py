ac253a9c1488de55e8b09f16d2181e5f81318ae4
renga9695 <52701372+renga9695@users.noreply.github.com>
2019-12-12 18:48:15 +0530

    version 1.1

--- hivecontent.py
+++ hivecontent.py
@@ -3,6 +3,6 @@ from pyspark.sql import SparkSession
 renga = SparkSession.builder.appName("ahamed").master("yarn-client").enableHiveSupport().getOrCreate()
 
 renga.sql("use jarvis")
-amma= renga.sql("select * from crime3 where primary_type = 'THEFT' limit 10")
+amma= renga.sql('select * from crime3 where primary_type = "THEFT" limit 10')
 amma.show()
 
