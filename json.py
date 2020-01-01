e04de24515d40a01056127ffc7e4f14a1a219dd3
renga9695 <52701372+renga9695@users.noreply.github.com>
2020-01-01 20:18:23 +0530

    version 1.2

--- json.py
+++ json.py
@@ -3,8 +3,8 @@ from pyspark.sql import SparkSession
 renga = SparkSession.\
         builder.\
         appName("json_processing").\
-        master("yarn").\
+        master("yarn-client").\
         getOrCreate()
 
-amma= renga.read.json("/user/shashankbh/jarvis/git/2019-8.json.gz")
+amma= renga.read.json("/user/shashankbh/jarvis/git/2019-*.json.gz")
 amma.show()
